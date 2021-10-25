c20520bb97f5b8766d0218c3a2b7b26211ff5aa5
Ivan Martins <91843801+ivanmartins090317@users.noreply.github.com>
2021-10-25 13:48:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ function calc(){
   var m =((n1 * n2)/ 0.47)/1000
   var beg = ((n1 * n2)/ 0.43)/1000
   var avanc = ((n1 * n2)/ 0.50)/1000
- 
-
+  
   res.innerHTML = `<strong>${beg.toFixed(2)}</strong> litros (Iniciantes)
   <strong>${m.toFixed(2)}</strong> litros (Intermediario)<strong>   
-  ${avanc.toFixed(2)}</strong> litros (Avançado)` 
+  ${avanc.toFixed(2)}</strong> litros (Avançado)`
 }
 function calc()
 
